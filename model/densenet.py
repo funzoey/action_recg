37e36be350e00ce072e92e9ccb4557aed82fcadd
action_recg <769077086@qq.com>
2022-10-07 17:58:51 +0800

    1

--- model/densenet.py
+++ model/densenet.py
@@ -1,12 +1,16 @@
 from turtle import forward
+from typing import OrderedDict
 import torch
 
 class Densenet():
     def __init__(self, out_channel):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(3, out_channel, 7, kernel_size=7, stride=2)
-        self.maxpool = torch.nn.MaxPool2d()
-        
+        self.features = torch.nn.Sequential(OrderedDict([
+            ("conv0", torch.nn.Conv2d(3, out_channel, 7, stride=2)),
+            ("BN", torch.nn.BatchNorm2d(out_channel)),
+            ()
+        ]))
+
     def forward(self, x):
         
         pass
