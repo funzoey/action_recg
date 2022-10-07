f48b0dc3ebb46436398be22ba853644e7a057e57
action_recg <769077086@qq.com>
2022-10-07 16:11:19 +0800

    1

--- model/densenet.py
+++ model/densenet.py
@@ -6,6 +6,7 @@ class Densenet():
         super().__init__()
         self.conv1 = torch.nn.Conv2d(3, out_channel, 7, kernel_size=7, stride=2)
         self.maxpool = torch.nn.MaxPool2d()
+        
     def forward(self, x):
         
         pass
