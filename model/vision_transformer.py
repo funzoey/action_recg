a3cea95932da896aae832d54891f83e71cb7c804
a <769077086@qq.com>
2022-11-06 14:30:44 +0800

    lab vit

---
+++ model/vision_transformer.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from torchvision.models import vision_transformer
+
+class Net(nn.Module):
+    def __init__(self, num_classes:int, pretrain):
+        super().__init__()
+        self.fc = nn.Linear(1000, num_classes)
+        self.vit = vision_transformer.VisionTransformer(image_size=224,
+                                                        patch_size=16,
+                                                        num_layers=12,
+                                                        num_heads=12,
+                                                        hidden_dim=768,
+                                                        mlp_dim=3072)
+        if pretrain:
+            self.vit.load_state_dict(torch.load(pretrain))
+
+    def forward(self, x):
+        _1000 = self.vit(x)
+        y = self.fc(_1000)
+        return y
--- utils/model_builder.py
+++ utils/model_builder.py
@@ -2,7 +2,7 @@ from unicodedata import name
 import torch
 import model.Resnet as rs
 import model.dense_clasifier as dense_clasifier
-from torchvision.models import vision_transformer
+import model.vision_transformer as Vit
 def build_model(args):
     CNN = args['CNN']
     if  CNN['name']== '':
@@ -15,10 +15,5 @@ def build_model(args):
         return dense_clasifier.densenet161(num_classes = int(CNN['class_num']))
 
     elif CNN['name'] == 'vitb16':
-        return vision_transformer.VisionTransformer(image_size=224,
-                                                    patch_size=16,
-                                                    num_layers=12,
-                                                    num_heads=12,
-                                                    hidden_dim=768,
-                                                    mlp_dim=3072,
-                                                    num_classes=CNN['class_num'])
+        model = Vit.Net(CNN['class_num'], './weights/' + CNN['pretrain'])
+        return model
